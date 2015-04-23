8bb8379f5afacb7c50b0989723cd931c51945d77
kaitlinmuth <kaitlin.muth@gmail.com>
2015-04-23 15:59:09 -0500

    Initial commit

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-//Javascript goes here
+$(document).ready(function(){
+	$("#generator").click(function(){
+		writeLine();
+	});
+
+	$("#content").on('click', "#changeColor", function(){
+		$(this).closest("#newLine").toggleClass("selected");
+	
+	});
+	$("#content").on('click', "#remove", function(){
+		$(this).closest("#newLine").remove();
+	});
+});
+
+var lineCount =0 ;
+	
+function writeLine(){
+	$("#content").append("<div id='newLine'> Line #"+lineCount +"</div>");
+	$("#content").children().last().append("<button id='changeColor'>Change Color</button>");
+	$("#content").children().last().append("<button id='remove'>Remove</button>");
+	lineCount++;
+}
